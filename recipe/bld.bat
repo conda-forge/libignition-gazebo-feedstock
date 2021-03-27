2495e1e3a56d163df04e84cf2435d204bf0e0217
Silvio Traversaro <silvio@traversaro.it>
2021-03-27 17:44:57 +0100

    Create bld.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,25 @@
+mkdir build
+cd build
+
+cmake ^
+    -G "Ninja" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True ^
+    -DCMAKE_CXX_STANDARD=17 ^
+    -DBUILD_TESTING=ON ^
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+:: Test
+ctest --output-on-failure -C Release -E "check_"
+if errorlevel 1 exit 1
