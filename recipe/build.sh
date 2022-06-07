74ba101abf2d72df3094c9918892cd84fd4f1e01
Silvio Traversaro <silvio@traversaro.it>
2022-06-07 08:51:27 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
+fi
+
 mkdir build
 cd build
 
